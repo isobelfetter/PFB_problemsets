f386d1bff420b0508238d5325f1cc8f0859a5fe1
pfb2024 <pfb2024@info25.cshl.edu>
2024-10-15 15:49:56 -0400

    added <50 test

--- python1/if_else_elif.py
+++ python1/if_else_elif.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 import sys
-number = -17
+number = 17
 if number > 0:
   print("positive")
+  if number < 50:
+    print("less than 50")
 elif number < 0:
   print("negative")
 elif number == 0:
