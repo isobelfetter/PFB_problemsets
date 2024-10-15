782eae5f736ca70cfeb8587512b4172b41994d7c
pfb2024 <pfb2024@info25.cshl.edu>
2024-10-15 16:14:49 -0400

    <50 testing

--- python1/number_testing_sys_argv.py
+++ python1/number_testing_sys_argv.py
@@ -2,6 +2,11 @@
 import sys
 number = int(sys.argv[1])
 print(f"The number being tested is {number}.")
-print(type(number))
 if number > 0:
   print(f"{number} is positive.")
+  if number < 50:
+    print(f"{number} is less than 50.")
+elif number < 0:
+  print(f"{number} is negative.")
+else:
+  print(f"{number} is zero.")
