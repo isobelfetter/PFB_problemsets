a71fd1482264d281eddfb8fc782dfd760c76e805
pfb2024 <pfb2024@info25.cshl.edu>
2024-10-15 16:00:42 -0400

    added divisible by 3 test

--- python1/if_else_elif.py
+++ python1/if_else_elif.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
-number = 18
+number = 60
 if number > 0:
   print("positive")
   if number < 50:
     print("less than 50")
     if number%2 == 0:
       print("it is an even number that is smaller than 50")
+  elif number > 50:
+    print("greater than 50")
+    if number%3 == 0:
+      print("it is larger than 50 and divisible by 3")
+  else:
+    print("number is 50")
 elif number < 0:
   print("negative")
-elif number == 0:
+else:
   print("zero")
