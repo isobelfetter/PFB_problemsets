d7957c69d130e28e822833aece9cb307c3d10e2e
pfb2024 <pfb2024@info25.cshl.edu>
2024-10-15 16:16:18 -0400

    even testing

--- python1/number_testing_sys_argv.py
+++ python1/number_testing_sys_argv.py
@@ -6,6 +6,8 @@ if number > 0:
   print(f"{number} is positive.")
   if number < 50:
     print(f"{number} is less than 50.")
+    if number%2 == 0:
+      print(f"{number} is even.")
 elif number < 0:
   print(f"{number} is negative.")
 else:
