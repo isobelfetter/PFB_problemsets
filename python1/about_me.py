8838e4f3e338e4ba1a9d6ca59b9b8a5ed0f7f6dc
pfb2024 <pfb2024@info25.cshl.edu>
2024-10-15 11:44:35 -0400

    added new line to f-string

--- python1/about_me.py
+++ python1/about_me.py
@@ -3,10 +3,10 @@ name = "Isobel"
 fav_color = "Blue"
 fav_activity = "Baking"
 fav_animal = "Sea Otter (Enhydra lutris)"
+new_line = "\n"
 print("My name: Isobel")
 print("My favorite color:", fav_color)
 print("My favorite activity:", fav_activity)
 print("My favorite animal:", fav_animal)
 print("My name is", name, ", my favorite color is", fav_color, ", and my favorite animal is", fav_animal)
-print(f"My name: {name}")
-print(f"My name is {name}, my favorite color is {fav_color}, my favorite activity is {fav_activity}, and my favorite animal is {fav_animal}.")
+print(f"My name is {name}, my favorite color is {fav_color}, my favorite activity is {fav_activity},{new_line}and my favorite animal is {fav_animal}.")
