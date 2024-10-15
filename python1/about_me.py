9e80f2a9aaedf9ab68cefa8288540cf1940c6e7c
pfb2024 <pfb2024@info25.cshl.edu>
2024-10-15 11:58:03 -0400

    added sys.argv

--- python1/about_me.py
+++ python1/about_me.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import sys
 name = "Isobel"
 fav_color = "Blue"
 fav_activity = "Baking"
@@ -10,3 +11,9 @@ print("My favorite activity:", fav_activity)
 print("My favorite animal:", fav_animal)
 print("My name is", name, ", my favorite color is", fav_color,", my favorite activity is", fav_activity, ", and my favorite animal is", fav_animal)
 print(f"My name is {name}, my favorite color is {fav_color}, my favorite activity is {fav_activity},{new_line}and my favorite animal is {fav_animal}.")
+name_sys = sys.argv[1]
+color_sys = sys.argv[2]
+activity_sys = sys.argv[3]
+animal_sys = sys.argv[4]
+print(name_sys,color_sys,activity_sys,animal_sys)
+print(name_sys+color_sys+activity_sys+animal_sys)
