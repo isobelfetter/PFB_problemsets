72c40c87ab11f6e2547da8ab2dfa87e8c976e4ed
pfb2024 <pfb2024@info25.cshl.edu>
2024-10-15 11:22:18 -0400

    added fave animal

--- python1/about_me.py
+++ python1/about_me.py
@@ -2,6 +2,8 @@
 name = "Isobel"
 fav_color = "Blue"
 fav_activity = "Baking"
+fav_animal = "Sea Otter (Enhydra lutris)"
 print("My name: Isobel")
 print("My favorite color:", fav_color)
 print("My favorite activity:", fav_activity)
+print("My favorite animal:", fav_animal)
