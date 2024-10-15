f532482373bb984d1387b40be81affc908c8e82d
pfb2024 <pfb2024@info25.cshl.edu>
2024-10-15 15:54:32 -0400

    added even test

--- python1/if_else_elif.py
+++ python1/if_else_elif.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-import sys
-number = 17
+number = 18
 if number > 0:
   print("positive")
   if number < 50:
     print("less than 50")
+    if number%2 == 0:
+      print("it is an even number that is smaller than 50")
 elif number < 0:
   print("negative")
 elif number == 0:
