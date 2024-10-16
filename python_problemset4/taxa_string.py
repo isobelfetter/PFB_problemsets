3b6185c7c26d4f1a34141f41e070b61754e48b5d
pfb2024 <pfb2024@info25.cshl.edu>
2024-10-16 16:30:56 -0400

    added sorting

--- python_problemset4/taxa_string.py
+++ python_problemset4/taxa_string.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
 taxa_string = "sapiens : erectus : neanderthalensis"
 print("string:", taxa_string)
+taxa_list = taxa_string.split(" : ")
+print(taxa_list)
+print(taxa_list[1])
+print(type(taxa_string))
+print(type(taxa_list))
+taxa_list.sort()
+print(taxa_list)
