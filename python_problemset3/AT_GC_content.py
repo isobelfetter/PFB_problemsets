d1fd0bee011ea063cfd659aa81a08233087fa15e
pfb2024 <pfb2024@info25.cshl.edu>
2024-10-16 13:40:17 -0400

    summing A and T

--- python_problemset3/AT_GC_content.py
+++ python_problemset3/AT_GC_content.py
@@ -2,4 +2,8 @@
 import sys
 sequence = sys.argv[1]
 print("Sequence being counted:",sequence)
-
+sequence_upper = sequence.upper()
+count_A = sequence_upper.count("A")
+count_T = sequence_upper.count("T")
+print(count_A, count_T)
+print(f'{count_A+count_T}')
