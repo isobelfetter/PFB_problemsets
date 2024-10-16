be282897957d3bdee68040f8d450068a9e99647f
pfb2024 <pfb2024@info25.cshl.edu>
2024-10-16 19:37:47 -0400

    summing odds and evens

---
+++ python_problemset4/for_loop_practice.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+numbers = [101,2,15,22,95,33,2,27,72,15,52]
+for num in numbers:
+  if num%2 == 0:
+    print(num)
+
+sorted_numbers = sorted(numbers)
+print(sorted_numbers)
+for num in sorted_numbers:
+  print(num)
+evens = [num for num in sorted_numbers if num%2 == 0]
+print("evens:", evens)
+even_sum = 0
+for num in evens:
+  even_sum = even_sum + num
+print("even sum:", even_sum)
+odds = [num for num in sorted_numbers if num%2 != 0]
+print("odds:", odds)
+odd_sum = 0
+for num in odds:
+  odd_sum = odd_sum + num
+print("odd sum:", odd_sum)
