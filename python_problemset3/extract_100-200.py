31b78d92e8e3c430d100a9697de9c86acd6c849c
pfb2024 <pfb2024@info25.cshl.edu>
2024-10-16 15:55:36 -0400

    added G counting

--- python_problemset3/extract_100-200.py
+++ python_problemset3/extract_100-200.py
@@ -2,5 +2,8 @@
 import sys
 sequence = sys.argv[1]
 print("Original sequence:", sequence)
-sub_sequence = sequence[99:200]
+sequence_upper = sequence.upper()
+sub_sequence = sequence_upper[99:200]
 print("100-200:",sub_sequence)
+count_G = sub_sequence.count("G")
+print("Number of 'G's:",count_G)
