de867231a2295d786a9ac70fb1381863a7245865
pfb2024 <pfb2024@info25.cshl.edu>
2024-10-16 11:39:36 -0400

    print sequence being counted

--- python_problemset3/nucleotidecounter.py
+++ python_problemset3/nucleotidecounter.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 import sys
 sequence = sys.argv[1]
-print(sequence)
+print("Sequence being counted:",sequence)
 sequence_upper = sequence.upper()
 count_A = sequence_upper.count("A")
 count_T = sequence_upper.count("T")
 count_C = sequence_upper.count("C")
 count_G = sequence_upper.count("G")
-print(count_A, count_T, count_G, count_C)
+print(f'{count_A} A\n{count_T} T\n{count_G} G\n{count_C} C')
