cb9d06306f3eb478f31ef8f71ee1dbb992c5eb5a
pfb2024 <pfb2024@info25.cshl.edu>
2024-10-16 13:49:55 -0400

    adding GC content

--- python_problemset3/AT_GC_content.py
+++ python_problemset3/AT_GC_content.py
@@ -5,7 +5,12 @@ print("Sequence being counted:",sequence)
 sequence_upper = sequence.upper()
 count_A = sequence_upper.count("A")
 count_T = sequence_upper.count("T")
+count_G = sequence_upper.count("G")
+count_C = sequence_upper.count("C")
 print(f'AT content is {count_A+count_T} bases.')
+print(f'GC content is {count_G+count_C} bases.')
 sequence_len = len(sequence_upper)
 count_AT = count_A + count_T
+count_GC = count_G + count_C
 print(f'AT content is {count_AT/sequence_len :.1%}.')
+print(f'GC content is {count_GC/sequence_len :.1%}.')
