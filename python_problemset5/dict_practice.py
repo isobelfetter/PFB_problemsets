b8638fade6d3cec36a1da105dfa56e1eff0ef022
pfb2024 <pfb2024@info25.cshl.edu>
2024-10-17 10:51:38 -0400

    adding user input

--- python_problemset5/dict_practice.py
+++ python_problemset5/dict_practice.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
 fav_dict = {'book' : 'The Martian', 'song' : 'Freebird', 'tree' : 'Eucalyptus'}
 print(fav_dict['book'])
+fav_thing = 'book'
+print(fav_dict[fav_thing])
+print(fav_dict['tree'])
+fav_dict['organism'] = "sea otter"
+fav_thing = 'organism'
+print(fav_dict[fav_thing])
+
+for fav in fav_dict:
+  print(fav, fav_dict[fav])
+
+import sys
+fav_thing = sys.argv[1]
+print(fav_dict[fav_thing])
