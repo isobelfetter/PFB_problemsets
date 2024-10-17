f2702a0a835c31156e6028c03fc1015f23979c1d
Isobel Fetter <isobelfetter@Isobels-MacBook-Pro.local>
2024-10-16 22:53:26 -0400

    finished reverse complement script

--- python_problemset3/reverse_complement.py
+++ python_problemset3/reverse_complement.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 import sys
 sequence = sys.argv[1]
-print("Original sequence:", sequence)
+sequence_upper = sequence.upper()
+translation_key = sequence_upper.maketrans("ATGC","TACG")
+complement = sequence_upper.translate(translation_key)
+reverse_comp = complement[::-1]
+print(f"{'Original Sequence':20}5'{sequence_upper} 3'\n{'Complement':20}3'{complement} 5'\n{'Reverse Complement':20}5'{reverse_comp} 3'")
