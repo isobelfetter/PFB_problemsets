9b77a974153994ef679de4674ba3747ba80fbe20
pfb2024 <pfb2024@info25.cshl.edu>
2024-10-18 21:02:00 -0400

    adding exception

--- python_problemset8/codons_3frames_from_fasta.py
+++ python_problemset8/codons_3frames_from_fasta.py
@@ -5,32 +5,37 @@ import re
 print(f'FASTA filepath:')
 file_input = input()
 fastaDict = {}
-with open(file_input, "r") as read_file:
-  for line in read_file:
-    line = line.rstrip()
-    if re.search(r"(^>(\S+)\s+(.*))", line):
-      found = re.search(r"(^>(\S+)\s+(.*))", line)
-      gene_name = found.group(2)
-      gene_name = re.sub(r">", r"", gene_name)
-      fastaDict[gene_name] = ""
-      line_partial = ""
-    else:
-      line_partial += line
-      codons = re.findall('[A-Z]{3}', line_partial)
-      fastaDict[gene_name] = line_partial
-codon_dict = {}
-for gene in fastaDict:
-  codon_dict[gene] = {}
-  codons1 = re.findall('[A-Z]{3}', line_partial)
-  codons2 = re.findall('[A-Z]{3}', line_partial[1:])
-  codons3 = re.findall('[A-Z]{3}', line_partial[2:])
-  codon_dict[gene]['frame-1-codons'] = codons1
-  codon_dict[gene]['frame-2-codons'] = codons2
-  codon_dict[gene]['frame-3-codons'] = codons3
+try:
+  with open(file_input, "r") as read_file:
+    for line in read_file:
+      line = line.rstrip()
+      if re.search(r"(^>(\S+)\s+(.*))", line):
+        found = re.search(r"(^>(\S+)\s+(.*))", line)
+        gene_name = found.group(2)
+        gene_name = re.sub(r">", r"", gene_name)
+        fastaDict[gene_name] = ""
+        line_partial = ""
+      else:
+        line_partial += line
+        codons = re.findall('[A-Z]{3}', line_partial)
+        fastaDict[gene_name] = line_partial
+        codon_dict = {}
+    for gene in fastaDict:
+        codon_dict[gene] = {}
+        codons1 = re.findall('[A-Z]{3}', line_partial)
+        codons2 = re.findall('[A-Z]{3}', line_partial[1:])
+        codons3 = re.findall('[A-Z]{3}', line_partial[2:])
+        codon_dict[gene]['frame-1-codons'] = codons1
+        codon_dict[gene]['frame-2-codons'] = codons2
+        codon_dict[gene]['frame-3-codons'] = codons3
 
-write_file = open('Python_08.codons-3frames.nt', 'w')  
-for gene in codon_dict:
-    for frame in codon_dict[gene]:  
-      write_file.write(f'{gene}-{frame}\n{codon_dict[gene][frame]}\n')
-write_file.close()
-print(f'File written.')
+
+    write_file = open('Python_08.codons-3frames.nt', 'w')  
+    for gene in codon_dict:
+      for frame in codon_dict[gene]:  
+        write_file.write(f'{gene}-{frame}\n{codon_dict[gene][frame]}\n')
+    write_file.close()
+  print(f'File written.')
+
+except:    
+  print("Can't find file:" , file_input)
