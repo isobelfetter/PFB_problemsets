00ca204b77f7df406bb8e2a334446c267123e000
pfb2024 <pfb2024@info25.cshl.edu>
2024-10-22 16:44:56 -0400

    ApoI enzyme regex

---
+++ python_problemset7/ApoI_regex.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import re, sys
+
+file_input = sys.argv[1]
+fastaDict = {}
+line_partial = ""
+
+with open(file_input, 'r') as read_file:
+    for line in read_file:
+        line = line.rstrip()
+        if re.search(r"(^>(\S+))", line):
+            found = re.search(r"(^>(\S+))", line)
+            header = found.group(1)
+            line = re.sub(r">", r"", header)
+            fastaDict[line] = ""
+            seq_name = line
+            line_partial = ""
+        else:
+            line_partial += line
+            fastaDict[seq_name] = line_partial
+
+for seq_name in fastaDict:
+    sequence = fastaDict[seq_name]
+    for found in re.finditer('([A|G])AATT([C|T])', sequence):
+        start = found.start(0) + 1
+        end = found.end(0)
+        print(f'{start}-{end}')
