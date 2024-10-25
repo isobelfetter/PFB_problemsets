a36b7708fd856c0d805abe67941a712e6759e450
pfb2024 <pfb2024@info25.cshl.edu>
2024-10-24 20:40:58 -0400

    small update to enzyme DNA cutter

--- python_problemset7/enzyme_DNA_cutter.py
+++ python_problemset7/enzyme_DNA_cutter.py
@@ -5,15 +5,15 @@ line_count = 0
 enzyme_dict = {}
 
 with open('enzyme_list.txt', 'r') as read_file:
-    for line in read_file:
-        line_count += 1
-        if line_count > 10:
-            line = line.rstrip()
-            found = re.search(r'^(\S+).*?(\S+)$', line)
-            enzyme = found.group(1)
-            cut_site = found.group(2)
-            if '^' in cut_site:
-                enzyme_dict[enzyme] = cut_site
+  for line in read_file:
+    line_count += 1
+    if line_count > 10:
+      line = line.rstrip()
+      found = re.search(r'^(\S+).*?(\S+)$', line)
+      enzyme = found.group(1)
+      cut_site = found.group(2)
+      if '^' in cut_site:
+        enzyme_dict[enzyme] = cut_site
 
 fasta_file = sys.argv[1]
 enzyme = sys.argv[2]
@@ -34,23 +34,25 @@ with open(fasta_file, "r") as read_file:
       line_partial += line
       fastaDict[seq_name] = line_partial
 
-pattern = enzyme_dict[enzyme]
-degen_dict = {"W":"[AT]", "S":"[CT]", "M":"[AC]", "K":"[GT]", "R":"[AG]", "Y":"[CT]", "B":"[CGT]", "D":"[AGT]", "H":"[ACT]", "V":"[ACG]", "N":"[ACGT]"}
-for degen in degen_dict:
-  if degen in pattern:
-    pattern = pattern.replace(degen, degen_dict[degen])
-
-pattern_no_cut = re.sub(r'\^', '', pattern)   
-
-for seq_name in fastaDict:
+if enzyme not in enzyme_dict:
+  print(f'{enzyme} cuts sticky ends')
+else:
+  pattern = enzyme_dict[enzyme]
+  degen_dict = {"W":"[AT]", "S":"[CT]", "M":"[AC]", "K":"[GT]", "R":"[AG]", "Y":"[CT]", "B":"[CGT]", "D":"[AGT]", "H":"[ACT]", "V":"[ACG]", "N":"[ACGT]"}
+  for degen in degen_dict:
+    if degen in pattern:
+      pattern = pattern.replace(degen, degen_dict[degen])
+    pattern_no_cut = re.sub(r'\^', '', pattern)   
+  for seq_name in fastaDict:
     sequence = fastaDict[seq_name]
     if re.search(pattern_no_cut, sequence):
-        cut_sequence = re.sub(pattern_no_cut, pattern, sequence) #need to figure out how to go back to original sequence instead of subbing in degens
-
-print(cut_sequence)
-fragments = cut_sequence.split('^')
-print(f'Number of fragments: {len(fragments)}')
-print(fragments)
-sorted_fragments = sorted(fragments, key = len, reverse=True)
-for fragment in sorted_fragments:
-  print(fragment)
+      cut_sequence = re.sub(pattern_no_cut, pattern, sequence) #need to figure out how to go back to original sequence instead of subbing in degens
+      print(cut_sequence)
+      fragments = cut_sequence.split('^')
+      print(f'Number of fragments: {len(fragments)}')
+      print(fragments)
+      sorted_fragments = sorted(fragments, key = len, reverse=True)
+      for fragment in sorted_fragments:
+        print(fragment)
+    else:
+      print(f'{enzyme} cut site not found in sequence')
